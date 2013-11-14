fde96b2f8b5f9e61cbb5689bfb4401eee7d7601b
myun2 <myun2@nwhite.info>
2013-11-14 13:46:46 +0900

    flax/processor/add.hpp Add

---
+++ include/myun2/flax/processor/add.hpp
@@ -0,0 +1,17 @@
+#ifndef __github_com_myun2__flax__processor__addition_HPP__
+#define __github_com_myun2__flax__processor__addition_HPP__
+
+namespace myun2
+{
+	namespace flax
+	{
+		namespace proc
+		{
+			class add
+			{
+			};
+		}
+	}
+}
+
+#endif//__github_com_myun2__flax__processor__addition_HPP__
