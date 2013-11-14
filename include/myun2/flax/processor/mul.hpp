802ee49edadc52648c96b0cf06df5123ab5018b5
myun2 <myun2@nwhite.info>
2013-11-14 13:51:47 +0900

    sub, mul, div Add

---
+++ include/myun2/flax/processor/mul.hpp
@@ -0,0 +1,24 @@
+#ifndef __github_com_myun2__flax__processor__mul_HPP__
+#define __github_com_myun2__flax__processor__mul_HPP__
+
+namespace myun2
+{
+	namespace flax
+	{
+		namespace proc
+		{
+			template <typename LeftType, typename RightType>
+			class mul
+			{
+			private:
+				const LeftType& lvalue
+				const RightType& rvalue;
+			public:
+				mul(const LeftType& l, const RightType& r) : lvalue(l), rvalue(r) {}
+				LeftType exec() const { return lvalue * rvalue; }
+			};
+		}
+	}
+}
+
+#endif//__github_com_myun2__flax__processor__mul_HPP__
